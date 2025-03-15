f678e80eed617fca95fd5ecea0b460ee3de3c9bb
Engga <17691328+xprimen@users.noreply.github.com>
2025-03-15 07:12:05 +0700

    Beta Testing

--- src/main.js
+++ src/main.js
@@ -1,24 +1,11 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const urlParams = new URLSearchParams(window.location.search);
+const type = urlParams.get("type");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+if (type === "get") {
+  document.querySelector("#kotak").style.display = "block";
+}
 
-setupCounter(document.querySelector('#counter'))
+window.ReactNativeWebView &&
+  window.ReactNativeWebView.postMessage(
+    JSON.stringify({ type: "loaded", data: true })
+  );
